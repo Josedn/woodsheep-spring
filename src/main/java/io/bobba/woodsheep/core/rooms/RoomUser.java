362ef7575b66d0ac238fc6b152b816afc2405d94
Jose D <joserafaeldn@gmail.com>
2026-07-31 23:07:40 -0500

    RoomUser reimplement Player

--- src/main/java/io/bobba/woodsheep/core/rooms/RoomUser.java
+++ src/main/java/io/bobba/woodsheep/core/rooms/RoomUser.java
@@ -1,8 +1,11 @@
 package io.bobba.woodsheep.core.rooms;
 
 import io.bobba.catanatron.enums.Color;
+import io.bobba.catanatron.state.Action;
+import io.bobba.catanatron.state.GameState;
 import io.bobba.catanatron.state.Player;
 import io.bobba.woodsheep.core.users.User;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -25,5 +28,10 @@ public class RoomUser implements Player {
   @Override
   public boolean isBot() {
     return false;
+  }
+
+  @Override
+  public Action decide(GameState state, List<Action> playableActions) {
+    return playableActions.get(0);
   }
 }
